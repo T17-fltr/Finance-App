1abe2f31ae46e50eef868e3d3f5cc80393e0821f
Tobi Bamidele <bigtbamz2008@gmail.com>
2025-11-21 20:41:43 -0800

    Commit for last week

--- lib/widgets/total_value.dart
+++ lib/widgets/total_value.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class TotalValueSection extends StatelessWidget {
   const TotalValueSection({super.key});
 
- @override
+  @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -16,7 +17,9 @@ class TotalValueSection extends StatelessWidget {
             fontWeight: FontWeight.w400,
           ),
         ),
+
         SizedBox(height: 8),
+
         Row(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
@@ -49,6 +52,66 @@ class TotalValueSection extends StatelessWidget {
               ),
             ),
           ],
+        ),
+
+        SizedBox(height: 25),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            ActionButton(
+              icon: Iconsax.add,
+              label: "Deposit",
+            ),
+            ActionButton(
+              icon: Iconsax.send,
+              label: "Send",
+            ),
+            ActionButton(
+              icon: Iconsax.document,
+              label: "Earn",
+            ),
+            ActionButton(
+              icon: Iconsax.more,
+              label: "Swap",
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class ActionButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const ActionButton({
+    super.key,
+    required this.icon,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            color: Color(0xFF2A2A2A),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Icon(icon, color: Colors.white, size: 26),
+        ),
+        SizedBox(height: 8),
+        Text(
+          label,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+          ),
         ),
       ],
     );
